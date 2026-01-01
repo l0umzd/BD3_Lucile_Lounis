a3e21bcd1c513be03aa85467446dc5e29bfc4cbc
l0umzd <lu.mazenod@gmail.com>
2026-01-01 18:16:05 +0100

    Update connexion.php

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<?php
+   session_start() ;   
+?>
+<html>
+  <head><title>Connexion</title>
+  </head>
+  <body>
+    <h1 aligh="center">Connectez-vous</h1><br/><br/>
+    <?php
+      if(isset($_SESSION['pseudo']) AND $_SESSION['pseudo'] != ""){
+        die('Vous êtes déjà connecté. <a href="index.php">Retour à l\'accueil</a>');
+      }
+    ?>
+    <form action="connexion2.php" method="POST">
+      Pseudo: <input type="text" name="pseudo"><br/><br/>
+      Mot de passe: <input type="password" name="pass"><br/><br/>
+      <input type="submit" value="Envoyer">   <input type="reset" value="Effacer">
+    </form>
+    <br/>
+    <br/>
+    <a href="index.php">Retour à l'index</a>
+  </body>
+</html>
