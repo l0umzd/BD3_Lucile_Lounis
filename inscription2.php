3e02c95b6646588dbdcec3298bcea20b3986e82a
l0umzd <lu.mazenod@gmail.com>
2026-01-01 18:03:40 +0100

    Update inscription2.php

--- inscription2.php
+++ inscription2.php
@@ -10,6 +10,35 @@
       die('Vous êtes déjà connecté. <a href="index.php">Retour à l\'accueil</a>');
     }
   ?>
+
+  <?php
+    if (empty($_POST["pseudo"]) or empty($_POST["pass1"]) or empty($_POST["pass2"]) or empty($_POST["mail"])) {
+		die("Vous devez remplir TOUS les champs !");
+	}
+    else {
+		$pseudo = $_POST["pseudo"];
+		$pass1 = $_POST["pass1"];
+		$pass2 = $_POST["pass2"];
+		$mail = $_POST["mail"];
+
+		if ($pass1 != $pass2) {
+			die("Les deux mots de passe doivent être identiques !");
+		}
+		else {
+			$connexion=mysqli_connect("mysql03.univ-lyon2.fr", "php_lmazenod", "7grcLredXvjBIAXGl1fAfaKPW");
+			mysqli_select_db($connexion,"closet_db");
+
+			$req = "INSERT INTO utilisateurs (Pseudo, Email, Mdp) VALUES ('".$pseudo."', '".$mail."', '".$pass1."')";
+
+			mysqli_query($connexion, $req);
+
+			mysqli_close($connexion);
+
+			echo 'Vous avez bien té enregisté.e avec le pseudo '.$pseudo.' et le mot de passe '.$pass1.' avec le mail '.$mail.'.';
+			echo '<br/><a href="index.php">Retour à l\'accueil</a>';
+		}
+	}
+  ?>
 </body>
   
 </html>
