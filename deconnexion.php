b4f16ef4ad29baed38bdd83362bda1dd28de7230
l0umzd <lu.mazenod@gmail.com>
2026-01-01 18:46:48 +0100

    Add logout functionality in deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,15 @@
+<?php
+	session_start();
+?>
+<html>
+<head><title>Deconnexion</title></head>
+<body bgcolor="#f9e6fe" text="191970">
+<h1 align="center">Déconnexion</h1>
+<?php
+	$_SESSION = array();
+	session_destroy();
+?>
+Vous avez bien été déconnecté.<br/><br/>
+<a href="index.php">Retour à l'accueil</a>
+</body>
+</html>
