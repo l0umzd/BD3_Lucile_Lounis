ad6bb2ca2a98db9c3431db322ba758a966f052a9
l0u <lu.mazenod@gmail.com>
2026-01-10 18:30:48 +0100

    Refactor SQL queries and add title validation

--- creationtenue2.php
+++ creationtenue2.php
@@ -15,43 +15,42 @@ mysqli_select_db($connexion,"closet_db");
 if (!isset($_POST['items']) || empty($_POST['items'])) {
     die("Aucun vêtement sélectionné.");
 }
-
+if (empty($_POST['Titre_Tenue'])) {
+    die("Le titre est obligatoire.");
+}
 $vetements = $_POST['items'];
 
 $Id_User = $_SESSION['Id_User'];
-$Titre_Tenue = $_POST['titre'] ?? 'Ma tenue';
+$Titre_Tenue = $_POST['Titre_Tenue'];
 $Id_Saison = $_POST['Id_Saison'] ?? 1;
 
 
-$sql = "INSERT INTO tenue (Titre_Tenue, Img_Tenue, Id_User, Id_Saison)
-        VALUES (?, '', ?, ?)";
-$stmt = mysqli_prepare($connexion, $sql);
-mysqli_stmt_bind_param($stmt, "sii", $Titre_Tenue, $Id_User, $Id_Saison);
-mysqli_stmt_execute($stmt);
+$req = "INSERT INTO tenue (Titre_Tenue, Img_Tenue, Id_User, Id_Saison)
+        VALUES ('$Titre_Tenue', '', $Id_User, $Id_Saison)";
+mysqli_query($connexion, $req);
+
 
 $Id_Tenue = mysqli_insert_id($connexion);
 
-$sql_link = "INSERT INTO tenue_vetement(Id_Tenue, Id_Vet) VALUES (?, ?)";
-$stmt_link = mysqli_prepare($connexion, $sql_link);
 
 foreach($vetements as $Id_Vet){
-  mysqli_stmt_bind_param($stmt_link, "ii", $Id_Tenue, $Id_Vet);
-  mysqli_stmt_execute($stmt_link);
+  $req2 = "INSERT INTO tenue_vetement (Id_Tenue, Id_Vet) VALUES ($Id_Tenue, $Id_Vet)";
+  $res2 = mysqli_query($connexion, $req2);
 }
 ?>
-<h2>Votre tenue</h2>
-<div class="outfit">
+<h2>Votre tenue: </h2> <?php echo htmlspecialchars($Titre_Tenue); ?>
+<div class="tenue">
 <?php
-$req = "
+$req3 = "
 SELECT v.Img_Vet
 FROM vetement v
 JOIN tenue_vetement tv ON v.Id_Vet = tv.Id_Vet
 WHERE tv.Id_Tenue = $Id_Tenue
 ";
 
-$res = mysqli_query($connexion, $req);
+$res3 = mysqli_query($connexion, $req3);
 
-while ($row = mysqli_fetch_assoc($res)) {
+while ($row = mysqli_fetch_assoc($res3)) {
     echo '<img src="'.$row['Img_Vet'].'" width="150">';
 }
 ?>
